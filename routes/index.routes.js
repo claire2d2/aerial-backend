2a85667b8c306b2b19224cc7df5d37b066aa118c
Claire Song <claireyuan.song@gmail.com>
2024-04-16 12:12:09 +0200

    added figure routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 
+//! all routes here are prefixed with /api
+
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
+
+router.use("/figures", require("./figures.routes.js"));
 
 module.exports = router;
