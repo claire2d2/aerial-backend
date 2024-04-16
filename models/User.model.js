0d5f652cf587ecc0b5886eade2e8b1bd4164f70e
Claire Song <claireyuan.song@gmail.com>
2024-04-16 11:19:19 +0200

    initial commit

---
+++ models/User.model.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required."],
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required."],
+    },
+    image: {
+      type: String,
+      // TODO : upload default image onto cloudinary
+      default:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1200px-Default_pfp.svg.png",
+    },
+    roles: {
+      type: String,
+      enum: ["user", "admin", "moderator"],
+      default: "user",
+    },
+    darkModePref: {
+      type: String,
+      enum: ["system", "light", "dark"],
+      default: "system",
+    },
+    filterHistPref: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
