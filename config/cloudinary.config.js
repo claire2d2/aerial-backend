67596eb44a15528618f5b43d7f416af7a765b1af
Claire Song <claireyuan.song@gmail.com>
2024-04-18 14:10:10 +0200

    cloudinary setup

---
+++ config/cloudinary.config.js
@@ -0,0 +1,20 @@
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const multer = require("multer");
+
+cloudinary.config({
+  api_key: process.env.CLOUDINARY_API_KEY,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "aerialAPI",
+  },
+});
+
+const parser = multer({ storage: storage });
+
+module.exports = parser;
